32d7aab037d927581c9a910c3cb8886184b18838
DerlisKevinA <160941328+DerlisKevinA@users.noreply.github.com>
2024-04-16 01:33:58 -0400

    Agregar archivos de proyecto.

---
+++ Galaga_USFX_L02/Intermediate/Build/Win64/Galaga_USFX_L02Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/LENOVO/Desktop/dkam/Galaga_USFX_L02/Galaga_USFX_L02/Intermediate/Build/Win64/Galaga_USFX_L02Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Galaga_USFX_L02/Source/Galaga_USFX_L02/Galaga_USFX_L02GameMode.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Galaga_USFX_L02GameMode.h"
+#include "Galaga_USFX_L02Pawn.h"
+
+AGalaga_USFX_L02GameMode::AGalaga_USFX_L02GameMode()
+{
+	// set default pawn class to our character class
+	DefaultPawnClass = AGalaga_USFX_L02Pawn::StaticClass();
+}
+
